f6f1d5e788c6af5b44e21b65dc6f4df8497deddf
Jcook894 <Jcook894@icloud.com>
2016-11-28 22:56:30 -0500

    Added functionality to the search box.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
-var articleRequest ="Julian";
-
-
- function MyViewModel() {
+function MyViewModel(articles) {
   var self = this;
-  self.nameTest = ko.observable("Julian");
+  self.articleSearch = $('articleSearch').val();
+
 
 };
 
+function loadData(){
 
-var nyUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleRequest + '=&sort=newest&api-key=04af26cdd76f4b8da348f53553eadcb3';
+   var nyUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleRequest + '=&sort=newest&api-key=04af26cdd76f4b8da348f53553eadcb3';
 
-$.getJSON(nyUrl, function(data){
+    $.getJSON(nyUrl, function(data){
 
-  articles = data.response.docs;
-  console.log(articles);
-})
+      nyArticles = data.response.docs;
+
+      for(var i = 0; i < nyArticles.length; i++){
+        var articles = nyArticles[i];
+
+      }
+
+});
+
+
 
 
 ko.applyBindings(new MyViewModel());
+
+return false;
+}
+$('#search-content').submit(loadData);
